b5898fbab153587ca482334da1c97d3f59e152db
郑能量！ <1301691180@qq.com>
2018-05-23 16:39:39 +0800

    改了webpack配置错误

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -150,6 +150,21 @@ const webpackConfig = merge(baseWebpackConfig, {
       chunksSortMode: 'dependency',
       chunks: ['manifest', 'vendor', 'searchVideo']
     }),
+    new HtmlWebpackPlugin({
+      filename: config.build.videoPlay,
+      template: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency',
+      chunks: ['manifest', 'vendor', 'videoPlay']
+    }),
     // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
     // enable scope hoisting
